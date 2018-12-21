05ae05684ddd90ff630952d6bd34574e3b57fa07
Gil Katchko <gil.katchko@gmail.com>
2018-12-21 11:31:01 +0200

    Summary bug fix

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -4,20 +4,20 @@ import {connect} from 'react-redux'
 import selectExpenses from '../selectors/expenses'
 import expensesTotal from '../selectors/expenses-total'
 
-export const ExpenseSummary = ({expensesAmount, expensesTotal}) => (
+export const ExpenseSummary = ({expensesCount, totalSum}) => (
     <div>
     {
-        <h2> Viewing {expensesAmount} {expensesAmount === 1? 'expense' : 'expenses'} totalling {numeral(expensesTotal).format('$0,0.00')}</h2>
+        <h2> Viewing {expensesCount} {expensesCount === 1? 'expense' : 'expenses'} totalling {numeral(totalSum).format('$0,0.00')}</h2>
     }      
     </div>
 )
 
 const mapStateToProps = (state)=>{
     const selectedExpenses = selectExpenses(state.expenses, state.filters)
-    const expensesTotal = numeral(expensesTotal(props.expenses)/100)
+    const total = numeral(expensesTotal(selectedExpenses)/100)
     return {
-        expensesAmount: selectedExpenses.length,
-        expensesTotal: expensesTotal
+        expensesCount: selectedExpenses.length,
+        totalSum: total
     }
 }
 
--- src/tests/components/ExpenseSummary.test.js
+++ src/tests/components/ExpenseSummary.test.js
@@ -3,11 +3,11 @@ import {shallow} from 'enzyme'
 import {ExpenseSummary} from '../../components/ExpenseSummary'
 
 test('Should return viewing all expenses', ()=>{
-    const wrapper = shallow(<ExpenseSummary expensesAmount={12} expensesTotal={200} />)
+    const wrapper = shallow(<ExpenseSummary expensesAmount={12} totalSum={200} />)
     expect(wrapper).toMatchSnapshot()
 })
 
 test('Should return viewing 1 expense', ()=>{
-    const wrapper = shallow(<ExpenseSummary expensesAmount={1} expensesTotal={200} />)
+    const wrapper = shallow(<ExpenseSummary expensesAmount={1} totalSum={200} />)
     expect(wrapper).toMatchSnapshot()
 })
